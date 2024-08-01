fb57f60d3ed952461b65d56ddbe8fe2b17cae459
BanJeoker <38705074+BanJeoker@users.noreply.github.com>
2024-08-01 14:23:43 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -57,9 +57,7 @@ test_logloss = log_loss(y_test, y_test_pred_proba)
 fpr, tpr, _ = roc_curve(y_test, y_test_pred_proba)
 roc_auc = auc(fpr, tpr)
 
-plt.figure(figsize=(14, 5))
-
-plt.subplot(1, 3, 1)
+plt.figure(figsize=(8, 6))
 plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlim([0.0, 1.0])
@@ -68,24 +66,25 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC Curve')
 plt.legend(loc='lower right')
+plt.show()
 
 # Plot Precision-Recall Curve
 precision, recall, _ = precision_recall_curve(y_test, y_test_pred_proba)
-plt.subplot(1, 3, 2)
+
+plt.figure(figsize=(8, 6))
 plt.plot(recall, precision, color='blue', lw=2)
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.title('Precision-Recall Curve')
+plt.show()
 
 # Plot Confusion Matrix
 cm = confusion_matrix(y_test, y_test_pred)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
-plt.subplot(1, 3, 3)
+
+plt.figure(figsize=(8, 6))
 disp.plot(ax=plt.gca(), cmap=plt.cm.Blues)
 plt.title('Confusion Matrix')
-
-# Show plots
-plt.tight_layout()
 plt.show()
 
 # Print metrics
